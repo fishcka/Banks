6491c8f6005a702810bccd630fc60c51c59b98ee
ludmila-afanasenko <ludmila.afanasenko@sap.com>
2020-05-08 19:09:53 +0300

    externalized path name

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,24 @@ import java.nio.charset.Charset;
 import java.nio.charset.UnsupportedCharsetException;
 
 public class Main {
+
+    public static final String PATHNAME = "C://11java/Banks_Statistics_2020.csv";
+    public static final String CHARSET_NAME = "windows-1251";
+
     public static void main(String[] args) {
-        String pathname = "C://11java/Banks_Statistics_2020.csv";
-        String charsetName = "windows-1251";
         try {
-            File file = new File(pathname);
-            FileReader fr = new FileReader(file, Charset.forName(charsetName));
+            File file = new File(PATHNAME);
+            FileReader fr = new FileReader(file, Charset.forName(CHARSET_NAME));
             BufferedReader reader = new BufferedReader(fr);
             String line = reader.readLine();
             while (line != null) {
                 System.out.println(line);
                 line = reader.readLine();
             }
-        }catch (FileNotFoundException e) {
-            System.out.println("File not found " + pathname);
-        }catch (IOException e) {
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found " + PATHNAME);
+        } catch (IOException e) {
             e.printStackTrace();
-        }catch (UnsupportedCharsetException e) {
-            System.out.println("we can't find charset " + e.getMessage());
         }
     }
 }
